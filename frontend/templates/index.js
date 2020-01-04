59fba2ec113c97394fdb1a9f317c662e34991c2a
jacoulin <405563383@qq.com>
2020-01-04 17:37:30 +0800

    update

--- frontend/templates/index.js
+++ frontend/templates/index.js
@@ -13,7 +13,7 @@ let main = new Vue({
         currentTime: "00:00",
         duration: "00:00",
         result: "测试",
-        positionX: 0,
+        dragged: false,
     },
     methods: {
         play: function () {
@@ -25,6 +25,7 @@ let main = new Vue({
                 this.$refs['j-audio'].pause();
             }
         },
+
         cont: function (time, fmt) {
 
             let o = {
@@ -38,45 +39,58 @@ let main = new Vue({
             return fmt;
 
         },
+
         getDuration: function () {
             let duration = Math.floor(this.$refs['j-audio'].duration);
             this.duration = this.cont(duration, 'mm:ss');
         },
+
         getCurrentTime: function () {
             let currentTime = Math.floor(this.$refs['j-audio'].currentTime);
             let duration = Math.floor(this.$refs['j-audio'].duration);
+
+            console.log(this.$refs['j-audio'].currentTime);
 
             if (currentTime >= 1){
                 this.currentTime = this.cont(currentTime,'mm:ss');
             }
 
-            let rate = currentTime / duration * 100;
-            this.$refs['j-process-play'].style.width = rate + '%';
+            if (!this.dragged) {
+                let rate = currentTime / duration * 100;
+                this.$refs['j-process-play'].style.width = rate + '%';
+            }
 
             if (this.currentTime === this.duration){
                 this.played = false;
                 this.$refs['j-audio'].pause();
             }
         },
+
         setDuration: function (time) {
 
         },
+
         setCurrentTime: function (time) {
             this.currentTime = this.cont(time, 'mm:ss');
             this.$refs['j-audio'].currentTime = time;
         },
+
         screenMax: function () {
             j_video_screen.$el.width = 560;
         },
+
         screenMid: function () {
             j_video_screen.$el.width = 480;
         },
+
         screenMin: function () {
             j_video_screen.$el.width = 320;
         },
+
         open: function () {
             temp.$el.click();
         },
+
         upload: function () {
             // alert(this.$el.files[0]);
             console.log(this.$el.files);
@@ -92,13 +106,19 @@ let main = new Vue({
                 console.log(this.result);
             }
         },
+
         move: function (e) {
+
+            this.dragged = true;
+
             let tar = e.target;        //获取目标元素
 
             let el = this.$refs['j-process-play'];
 
             //算出鼠标相对元素的位置
             let disX = e.clientX - el.offsetWidth;
+
+            let rate = 0;
 
             document.onmousemove = (e)=>{       //鼠标按下并移动的事件
                 // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
@@ -112,11 +132,11 @@ let main = new Vue({
                     left = this.$refs['j-process-bar'].offsetWidth;
                 }
 
-                let rate = left / this.$refs['j-process-bar'].offsetWidth * 100;
+                rate = left / this.$refs['j-process-bar'].offsetWidth * 100;
 
                 this.$refs['j-process-bar'].style.height = '4px';
                 el.style.width = rate + '%';
-                this.setCurrentTime(Math.floor(rate * this.$refs['j-audio'].duration / 100) );
+
 
                 // 防止选择内容
                 window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
@@ -125,7 +145,10 @@ let main = new Vue({
             document.onmouseup = (e) => {
                 document.onmousemove = null;
                 document.onmouseup = null;
+                this.dragged = false;
+                this.setCurrentTime(Math.floor(rate * this.$refs['j-audio'].duration / 100) );
             };
-        }
+        },
+
     }
 });
